5f3668562778830dfc3e75bf174b8d6c720cee8a
felipe1998s <felipeandressuarez20@gmail.com>
2023-06-01 08:09:43 -0500

    second

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,10 +1,10 @@
 //componentes 
-import { Home } from "./Home/home";
-import { MyAccount } from "./MyAccount/myAccount";
-import { MyOrder } from "./MyOrder/myOrder";
-import { MyOrders } from "./MyOrders/myOrders";
+import { Home } from "./Home/Home";
+import { MyAccount } from "./MyAccount/MyAccount";
+import { MyOrder } from "./MyOrder/MyOrder";
+import { MyOrders } from "./MyOrders/MyOrders";
 import { SignIn } from "./SignIn/SignIn";
-import { NotFound } from "./NotFound/notFound";
+import { NotFound } from "./NotFound/NotFound";
 
 
 export { Home, MyAccount, MyOrder, MyOrders, NotFound, SignIn  }; 
